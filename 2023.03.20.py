925312b62596a050d4ec14fec8de2a119fe6aab8
wkddmswns <127712534+wkddmswns@users.noreply.github.com>
2023-03-20 15:43:31 +0900

    Add files via upload

---
+++ 2023.03.20.py
@@ -0,0 +1,80 @@
+a = 1234
+print(a,type(a))
+
+b = 12345.6789
+print(b,type(b))
+
+hex = 0xf54
+oct = 0o567
+bin = 0b1101
+print(hex,oct,bin)
+
+bl = True
+print(bl,type(bl))
+
+a = 10<20
+b = 10 == 10
+c = 10 !=10
+print(a,b,c)
+
+a = 10<= 20
+print(a)
+
+print("따옴표 ' 출력")
+print('따옴표 " 출력')
+print("같은따옴표\" 출력")
+
+a = 10
+b = 20
+print(a,"+",b,"=",a+b)
+
+a = 10
+b = 20
+print("%d" % a)
+print("%d" % (a+b))
+print("%d + %d = %d" %(a, b, a+b))
+
+int = 12345
+float = 12345.6789
+print("%10d" % int)
+print("%10.4d" % int)
+
+print("%10.4d" % float)
+
+print("%10.4f" % float)
+print("%20.10f" % float)
+print("%5.5f" % float)
+print("%5.2f" % float)
+print("%3.2f" % float)
+
+a = 10
+b = 20
+print(a, "+",b,"\n =",a+b)
+
+print(a, "\b+",b,"\n =",a+b)
+
+
+a = int (input("첫번째 정수를 입력하세요:"))
+b = int (input("두번째 정수를 입력하세요:"))
+c = a + b
+print("두 정수를 더한 값은 %d 입니다" % c)
+print(f"두 정수를 더한 값은 {c} 입니다")
+
+a="abc"
+b="def"
+C=3
+#print(a*b)
+print(a*c)
+
+a = int(input(" 첫번째 정수를 입력하세요:"))
+b = int(input("두번째 정수를 입력하세요:"))
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+
+    
+
+
