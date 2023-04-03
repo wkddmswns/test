c93ccbe6eb4e3f7745e4a05677a762e7d0889f38
wkddmswns <127712534+wkddmswns@users.noreply.github.com>
2023-04-03 15:00:31 +0900

    Add files via upload

---
+++ 0403.py
@@ -0,0 +1,65 @@
+# num = int(input("점수를 입력하세요"))
+
+# if num>=95-100:
+    # print("A+학점 입니다",num)
+# elif num>=90:
+    # print("A학점 입니다",num)
+# elif num>=80:
+    #  print("B학점 입니다",num)
+
+# elif num>=70:
+    #  print("C학점 입니다",num)
+
+# elif num>=60:
+    #  print("D학점 입니다",num)     
+# elif num>=59:
+    #  print("F학점 입니다",num)     
+    
+#   score = int(input("점수를입력하세요"))
+#  print(score)
+#  score = score//10
+#  match score:
+#    case 10 :
+#      print('A+')
+#    case 9 :
+#      print('A')
+#    case 8 :
+#      print('B')
+#    case 7 :
+#      print('C')
+#    case 6 :
+#     print('D')
+#    case _ :
+#     print('F')        
+
+
+# for i in range(10):
+#   print(i)
+
+# for i in range(3,10):
+#   print(i)
+# for i in range(1,3,1):
+#     print(f"{i}값 반복")
+
+# for i in range(1,6):
+#     print('*')
+# for i in range(10):
+#      print("*",end="*")
+# a = 0
+# for i in range(1,100):
+#     a = a+i
+#     print(a)    
+
+
+# a = int(input("구구단 3단을 출력하세요"))
+# for i in range(1,10):
+#     print(f"{a} * {i} = {a*i}")
+
+a = int(input("단수를 입력하세요 :"))
+for i in range(1,10):
+   print("f{a:2d} * {i:2d} = {a*i3d} ", end="")
+   if i%3 == 0:
+       print("")
+
+
+
