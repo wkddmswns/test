d8e77519f486e25cbc6a061b0d28e83ed308414b
wkddmswns <127712534+wkddmswns@users.noreply.github.com>
2023-04-11 16:05:46 +0900

    Add files via upload

---
+++ jyj3.cpp
@@ -0,0 +1,94 @@
+/*#include <iostream>
+using namespace std;
+
+void main()
+{
+
+
+	if (0)//바로 아래 한줄만 출력 0이면 2,3 1이면 123
+	{
+		cout << 1 << endl;
+		cout << 2 << endl;
+		cout << 3 << endl;
+	}
+}*/
+
+#include <iostream>
+using namespace std;
+
+/*#define SWAP(a,b) a^=b; b^=a; a^=b;
+
+void main()
+{
+	int a = 0;
+	int b = 1;
+
+	if (0)//거짓이라 원래 출력값은 스왑하지않고 0 1 이 나와야한다 그런데 1 1이나온이유가뭘까
+		SWAP(a, b);
+	cout << a << "" << b << endl;
+}*/
+
+
+/*20세 미만일시 학생 할인이라는 출력*/
+/*void main()
+
+ {
+	unsigned short age;
+	cout << "나이를 입력하세요"<<endl;
+
+	cin >> age;
+
+	if (age < 20) {
+		cout << "청소년 할인" << endl;
+	}
+}*/
+/*1000걸음이 넘으면 달성이라고 출력되는거*/
+/*void main()
+{
+	unsigned int run;
+		cout << "걸음수를 입력 하세요" << endl;
+		cin >> run;
+		if (run > 1000)
+		{
+			cout << "달성" << endl;
+		}
+}*/
+/* 짝수인지 홀수인지 판단하는것*/
+/*void main()
+{
+	unsigned short num;
+		cout << "정수를 입력하세요" << endl;
+		cin >> num;
+		if (num % 2 == 0)
+
+			cout << "짝수" << endl;
+		else
+			cout << "홀수" << endl;;
+
+
+
+}*/
+/*시간이 오후인지 오전인지 판단하는거*/
+/*void main()
+{
+	unsigned short time;
+	cout << "시간을 입력하세요" << endl;
+	cin >> time;
+	if (time<12)
+		cout << "오전" << endl;
+	else
+		cout << "오후" << endl;
+}*/
+
+/*3과5의배수인지 판단하는거*/
+/*void main()
+
+{
+	unsigned short num;
+	cout << "정수를 입력하세요" << endl;
+	cin >> num;
+	if (num % 3 == 0 && num % 5 == 0)
+		cout << "3과5의배수입니다" << endl;
+	else
+		cout << "3과 5의 배수가 아닙니다" << endl;
+}*/
