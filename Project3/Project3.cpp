f5d9b380732d94093531c21090b8dce2f5ea7654
wkddmswns <127712534+wkddmswns@users.noreply.github.com>
2023-09-04 15:59:24 +0900

    Add files via upload

---
+++ Project3/Project3.cpp
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+
+void main()
+{
+	int sum = 0;
+	int A = 1;
+	do {
+		if(A % 2 == 0)
+		sum = sum + A;
+		A = A + 1;	
+		
+	}	
+	while (A <= 100);
+	printf("% d", sum);
+}
+
+
+
+void main()
+{
+	int sum = 0;
+	int A = 1;
+	do {
+		if (A % 3 == 0)
+			sum = sum + A;
+		A = A + 1;
+
+	} while (A <= 100);
+	printf("% d", sum);
+}
+
+
+void main()
+{
+	int sum = 0;
+	int A = 1;
+	do {
+			sum = sum + A;
+		A = A + 1;
+
+	} while (A <= 100);
+	printf("% d", sum);
+}
